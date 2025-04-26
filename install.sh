c5558179864f75232ac5c39b7fae5eb748d951b2
Evaan Chowdhry <evaanchowdhry@gmail.com>
2025-04-26 19:06:43 -0400

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(uname)" != "Darwin" ]; then
+  echo "This script is intended for macOS only."
+  exit 1
+fi
+
+mkdir -p "/tmp/Tritium"
+if [ ! -d "/Applications/Tritium.app" ]; then
+  echo "Tritium is not installed. Proceeding with installation."
+else
+  echo "Tritium is already installed. Exiting."
+  exit 0
+fi
+echo "Downloading Tritium..."
+curl -L -o "/tmp/Tritium/Tritium.zip" "https://github.com/Phantom8015/Tritium/releases/download/v1.0.0/Tritium.zip"
+echo "Extracting Tritium..."
+unzip -o "/tmp/Tritium/Tritium.zip" -d "/tmp/Tritium"
+mv -f "/tmp/Tritium/Tritium.app" "/Applications"
+rm -rf "/tmp/Tritium"
+
+echo "Tritium has been successfully installed!"
